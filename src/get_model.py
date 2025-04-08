5dff0f5b81f3e3b7591781bccd2e3e0c1a73f698
alextree <alextreecn@gmail.com>
2025-04-07 21:13:56 -0700

    update model download dir

--- src/get_model.py
+++ src/get_model.py
@@ -3,6 +3,7 @@ from huggingface_hub import snapshot_download
 # In a Python REPL or script:
 snapshot_download(
     repo_id="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-    local_dir="../data/models",
+    local_dir="../data/hub/models--deepseek-ai--DeepSeek-R1-Distill-Qwen-1.5B",
+    local_dir_use_symlinks=False,
     resume_download=True
 )
